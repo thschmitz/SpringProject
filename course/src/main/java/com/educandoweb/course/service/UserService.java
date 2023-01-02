6b62b4bf56fd0ec6bc35b33dbc1fdbd836455865
thschmitz <thomas.henrique.schmitz@gmail.com>
2023-01-01 22:12:49 -0300

    DatabaseException Handling

--- course/src/main/java/com/educandoweb/course/service/UserService.java
+++ course/src/main/java/com/educandoweb/course/service/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.service.exceptions.DatabaseException;
 import com.educandoweb.course.service.exceptions.ResourceNotFoundException;
 
 @Service // Para poder ser injetado pelo autowired la dentro do UserResource;
@@ -31,7 +34,19 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		
+		/*try {
+			repository.deleteById(id);	
+		} catch(RuntimeException e) {
+			e.printStackTrace(); // nos retorna a excecao EmptyResultDataAccessException
+		}*/
+		try {
+			repository.deleteById(id);	
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage()); // todo
+		}
 	}
 	
 	public User update(Long id, User obj) {
