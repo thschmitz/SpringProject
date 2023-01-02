3cae24c80fb83b036610c8e31fd26d46a116d760
thschmitz <thomas.henrique.schmitz@gmail.com>
2023-01-01 21:53:52 -0300

    Exception Handling

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ef/d070b0e1358a001d1672b703498c6da2
@@ -0,0 +1,5 @@
+package com.educandoweb.course.resources.exceptions;
+
+public class StandardError {
+
+}
--- course/src/main/java/com/educandoweb/course/service/UserService.java
+++ course/src/main/java/com/educandoweb/course/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.service.exceptions.ResourceNotFoundException;
 
 @Service // Para poder ser injetado pelo autowired la dentro do UserResource;
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		
-		return obj.get(); // o get retorna algo do tipo User, denominado acima;
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); 
 	}
 	
 	public User insert(User obj) {
